7bee1a9fa2c1eaf97296c4cf26d9e8310083244f
micenturion <96074793+micenturion@users.noreply.github.com>
2022-10-06 16:35:07 -0300

    Update update-zimbra-centos7.sh

--- update-zimbra-centos7.sh
+++ update-zimbra-centos7.sh
@@ -3,4 +3,4 @@ yum clean metadata
 yum check-update
 yum install -y zimbra-common-core-jar zimbra-common-core-libs zimbra-mbox-store-libs > /tmp/install_update.txt
 #runuser -l zimbra -c 'zmcontrol restart'
-echo "Se requiere de su intervención, verifique si se han aplicado cambios en los paquetes en su servidor, es posible que los mismos requieran reiniciar los servicios"$(less /tmp/install_update.txt) | mail -vvv -s "Atencion" administrador@administrador.com
+echo "Se requiere de su intervención, verifique si se han aplicado cambios en los paquetes en su servidor, es posible que los mismos requieran reiniciar los servicios"$(less /tmp/install_update.txt) | mail -s "Atencion" administrador@administrador.com
