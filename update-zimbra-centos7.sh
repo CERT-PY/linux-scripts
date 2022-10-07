3962efc2d21f28251452df0be9db9412cf0722ba
micenturion <96074793+micenturion@users.noreply.github.com>
2022-10-07 10:30:51 -0300

    Add files via upload

---
+++ update-zimbra-centos7.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+var1="Nothing to do"
+yum clean metadata
+yum check-update zimbra*
+yum install -y zimbra-common-core-jar zimbra-common-core-libs zimbra-mbox-store-libs > /tmp/install_update.txt
+#runuser -l zimbra -c 'zmcontrol restart'
+if [[ $(grep -c "Nothing to do" /tmp/install_update.txt) = 0 ]]; then
+	echo -e "Se requiere de su intervención, verifique si se han aplicado cambios en los paquetes en su servidor, es posible que los mismos requieran reiniciar los servicios\n\nDetalles:\n\n".$(</tmp/install_update.txt) | mail -s "Atencion" mcenturion@cert.gov.py
+else
+	echo /dev/null
+fi
