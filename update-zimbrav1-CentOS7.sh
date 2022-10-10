4d207ed80e61ecd4ba1c7041c465ce2296d173ce
micenturion <96074793+micenturion@users.noreply.github.com>
2022-10-10 14:11:46 -0300

    Update update-zimbrav1-CentOS7.sh

--- update-zimbrav1-CentOS7.sh
+++ update-zimbrav1-CentOS7.sh
@@ -8,7 +8,7 @@ yum check-update zimbra*
 yum install -y zimbra* > /tmp/install_update.txt
 #Esta línea verifica si, se han instalado actualizaciones para el servidor de correo, verificando la salida del archivo de texto ubicado en la carpeta tmp, evalúa si en la salida se ha escrito la cadena de etexto "" si no encuentra coincidencia con la cadena de texto, el programa envía un correo a una cuenta específica para el dominio sobre el cual opera el servidor de correo con la salida del archivo "install_updates.txt"
 if [[ $(grep -c "Nothing to do\|Nada para hacer" /tmp/install_update.txt) = 0 ]]; then
-        { echo "Subject: Atencion" | echo "Se requiere de su intervención, verifique si se han aplicado cambios en los paquetes en su servidor, es posible que los mismos requieran reiniciar los servicios\n\nDetalles:\n\n".$(</tmp/install_update.txt) | /opt/zimbra/common/sbin/sendmail -v cuentadecorreo@dominio.com }
+        { echo "Se requiere de su intervención, verifique si se han aplicado cambios en los paquetes en su servidor, es posible que los mismos requieran reiniciar los servicios\n\nDetalles:\n\n".$(</tmp/install_update.txt) | /opt/zimbra/common/sbin/sendmail -v cuentadecorreo@dominio.com }
 else
         echo /dev/null
 fi
